66e1017ca1ba6a8c8f3467158d7b40abe0a57161
sanin <saninthottungalhere@gmail.com>
2026-01-18 18:45:01 +0530

    build showcase header created

--- lib/pages/builds_page.dart
+++ lib/pages/builds_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:ssnbuilds/extensions/context_ext.dart';
+import 'package:ssnbuilds/widgets/content_wrapper.dart';
 
 class BuildsPage extends StatelessWidget {
   const BuildsPage({super.key});
@@ -6,7 +8,20 @@ class BuildsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SliverMainAxisGroup(
-      slivers: [],
+      slivers: [
+        SliverToBoxAdapter(
+          child: ContentWrapper(
+            child: Text(
+              "Builds Showcase",
+              style: TextStyle(
+                fontFamily: 'minecraft_block',
+                fontSize: 24,
+                color: context.colorScheme.onPrimaryContainer,
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
