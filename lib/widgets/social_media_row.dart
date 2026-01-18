3a9639db4ae92892632ef57303db43fc7d557c3c
sanin <saninthottungalhere@gmail.com>
2026-01-18 13:26:30 +0530

    animation added on social media rows

--- lib/widgets/social_media_row.dart
+++ lib/widgets/social_media_row.dart
@@ -22,22 +22,69 @@ class SocialMediaRow extends StatelessWidget {
       children: List.generate(
         _socialMedias.length,
         (index) {
-          return GestureDetector(
-            behavior: HitTestBehavior.translucent,
+          return _SocialMediaIcon(
+            iconPath: _socialMedias[index].iconPath,
             onTap: () => _onTap(_socialMedias[index].url),
-            child: CircleAvatar(
-              radius: 16,
-              child: Image.asset(
-                _socialMedias[index].iconPath,
-                height: 26,
-                width: 26,
-                color: context.colorScheme.onPrimaryContainer.withValues(
-                  alpha: 0.6,
-                ),
-              ),
-            ),
           );
         },
+      ),
+    );
+  }
+}
+
+class _SocialMediaIcon extends StatefulWidget {
+  const _SocialMediaIcon({
+    required this.iconPath,
+    required this.onTap,
+  });
+
+  final VoidCallback onTap;
+  final String iconPath;
+
+  @override
+  State<_SocialMediaIcon> createState() => _SocialMediaIconState();
+}
+
+class _SocialMediaIconState extends State<_SocialMediaIcon> {
+  bool hightlight = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return MouseRegion(
+      onEnter: (_) => setState(() {
+        hightlight = true;
+      }),
+      onExit: (_) => setState(() {
+        hightlight = false;
+      }),
+      child: GestureDetector(
+        onTap: widget.onTap,
+        behavior: .translucent,
+        child: AnimatedContainer(
+          duration: const Duration(milliseconds: 200),
+          curve: Curves.easeInOut,
+          height: 32,
+          width: 32,
+          padding: EdgeInsets.all(hightlight ? 2 : 3),
+          decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: context.colorScheme.primaryContainer,
+          ),
+          child: TweenAnimationBuilder(
+            duration: const Duration(milliseconds: 200),
+            curve: Curves.easeInOut,
+            tween: ColorTween(
+              begin: context.colorScheme.onPrimaryContainer.withValues(
+                alpha: 0.6,
+              ),
+              end: context.colorScheme.onPrimaryContainer.withValues(
+                alpha: hightlight ? 1 : 0.6,
+              ),
+            ),
+            builder: (context, value, _) =>
+                Image.asset(widget.iconPath, color: value),
+          ),
+        ),
       ),
     );
   }
