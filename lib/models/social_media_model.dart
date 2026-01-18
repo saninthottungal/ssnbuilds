22b80e14cd6e94fc0edef425972d56ec8b39090d
sanin <saninthottungalhere@gmail.com>
2026-01-18 11:45:00 +0530

    social media model created

---
+++ lib/models/social_media_model.dart
@@ -0,0 +1,9 @@
+class SocialMediaModel {
+  SocialMediaModel({
+    required this.url,
+    required this.iconPath,
+  });
+
+  final String url;
+  final String iconPath;
+}
---
+++ lib/widgets/social_media_row.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_gutter/flutter_gutter.dart';
+
+class SocialMediaRow extends StatelessWidget {
+  const SocialMediaRow({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      spacing: context.gutter,
+      children: [],
+    );
+  }
+}
