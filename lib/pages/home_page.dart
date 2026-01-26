65de5a5f72bab0ac80009ebe67da9872f1d84f79
sanin <saninthottungalhere@gmail.com>
2026-01-26 11:33:29 +0530

    home header added

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,12 +1,41 @@
 import 'package:flutter/material.dart';
+import 'package:ssnbuilds/extensions/context_ext.dart';
+import 'package:ssnbuilds/widgets/content_wrapper.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const SliverToBoxAdapter(
-      child: Center(child: Text("Home Page")),
+    return SliverMainAxisGroup(
+      slivers: [
+        SliverToBoxAdapter(
+          child: ContentWrapper(
+            contentCrossAxisAlignment: .start,
+            child: Column(
+              crossAxisAlignment: .start,
+
+              children: [
+                Text(
+                  "For starters,",
+                  style: context.textTheme.headlineSmall,
+                ),
+                Text(
+                  "I am sanin T.",
+                  style: context.textTheme.displayLarge?.copyWith(
+                    fontFamily: 'minecraft_block',
+                    color: context.colorScheme.onPrimaryContainer,
+                  ),
+                ),
+                Text(
+                  "A Software Developer.",
+                  style: context.textTheme.headlineSmall,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
