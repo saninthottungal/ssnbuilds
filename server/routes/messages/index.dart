df32dc4b11fc5ca3abda8b1fcaf3f5d5703ee1d8
sanin <saninthottungalhere@gmail.com>
2026-01-26 20:04:44 +0530

    methods setup done

--- server/routes/messages/index.dart
+++ server/routes/messages/index.dart
@@ -1,5 +1,17 @@
 import 'package:dart_frog/dart_frog.dart';
 
 Future<Response> onRequest(RequestContext context) async {
+  return switch (context.request.method) {
+    .get => await _get(context),
+    .post => await _post(context),
+    _ => Response(statusCode: 405),
+  };
+}
+
+Future<Response> _get(RequestContext context) async {
+  return Response();
+}
+
+Future<Response> _post(RequestContext context) async {
   return Response();
 }
