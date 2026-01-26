117b99c9396dd9f19b8863b0026456f96a7fe5b7
sanin <saninthottungalhere@gmail.com>
2026-01-26 11:56:16 +0530

    basic home setup done

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gutter/flutter_gutter.dart';
 import 'package:ssnbuilds/extensions/context_ext.dart';
 import 'package:ssnbuilds/widgets/content_wrapper.dart';
 
@@ -10,26 +11,73 @@ class HomePage extends StatelessWidget {
     return SliverMainAxisGroup(
       slivers: [
         SliverToBoxAdapter(
-          child: ContentWrapper(
-            contentCrossAxisAlignment: .start,
-            child: Column(
-              crossAxisAlignment: .start,
-
+          child: IntrinsicHeight(
+            child: Row(
+              spacing: context.gutter,
               children: [
-                Text(
-                  "For starters,",
-                  style: context.textTheme.headlineSmall,
-                ),
-                Text(
-                  "I am sanin T.",
-                  style: context.textTheme.displayLarge?.copyWith(
-                    fontFamily: 'minecraft_block',
-                    color: context.colorScheme.onPrimaryContainer,
+                Expanded(
+                  child: ContentWrapper(
+                    leading: Icons.safety_divider_outlined,
+                    title: 'Hello World🎉',
+                    contentCrossAxisAlignment: .start,
+                    child: Column(
+                      crossAxisAlignment: .start,
+                      children: [
+                        Text(
+                          "For starters,",
+                          style: context.textTheme.headlineSmall,
+                        ),
+                        Text(
+                          "I am sanin T.",
+                          style: context.textTheme.displayLarge?.copyWith(
+                            fontFamily: 'minecraft_block',
+                            color: context.colorScheme.onPrimaryContainer,
+                          ),
+                        ),
+                        Text(
+                          "A Software Developer.",
+                          style: context.textTheme.headlineSmall,
+                        ),
+                      ],
+                    ),
                   ),
                 ),
-                Text(
-                  "A Software Developer.",
-                  style: context.textTheme.headlineSmall,
+                Expanded(
+                  child: ContentWrapper(
+                    leading: Icons.toggle_on_outlined,
+                    title: 'What am i doing right now?',
+                    child: Column(
+                      spacing: context.gutterSmall,
+                      children: [
+                        const ContentWrapper(
+                          contentCrossAxisAlignment: .start,
+                          leading: Icons.book,
+                          title: "The book i'm reading",
+                          child: Text(
+                            '''"Code : The Hidden Language of Computer Hardware and Software" by Charles Petzold''',
+                          ),
+                        ),
+                        const ContentWrapper(
+                          contentCrossAxisAlignment: .start,
+                          leading: Icons.movie,
+                          title: "The Movie/Series i'm watching",
+                          child: Text("The Rookie SE04EP11"),
+                        ),
+                        const ContentWrapper(
+                          contentCrossAxisAlignment: .start,
+                          leading: Icons.auto_stories,
+                          title: "The topic i'm studying",
+                          child: Column(
+                            crossAxisAlignment: .start,
+                            children: [
+                              Text("API development using dart_frog"),
+                              Text("C programming basics"),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
                 ),
               ],
             ),
