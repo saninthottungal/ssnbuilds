dff26e25811278371eecc97a5cd4ebf82bf7513e
sanin <saninthottungalhere@gmail.com>
2026-05-06 22:33:29 +0530

    updated naming conventions

--- lib/constants/url_const.dart
+++ lib/constants/url_const.dart
@@ -2,7 +2,7 @@ class URLConsts {
   const URLConsts._();
 
   static const resume =
-      'https://raw.githubusercontent.com/saninthottungal/ssnbuilds/simple/public/resume/SANIN_RESUME.pdf';
+      'https://raw.githubusercontent.com/saninthottungal/ssnbuilds/simple/public/resume/sanin_resume.pdf';
   static const myGithub = 'https://github.com/saninthottungal';
   static const myTwitter = 'https://x.com/ssn564';
   static const myLinkedin = 'https://www.linkedin.com/in/saninthottungal/';
